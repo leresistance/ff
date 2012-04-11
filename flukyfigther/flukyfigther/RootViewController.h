b63a03960fc8f1ef965d8f92d631409c4c6eb43e
leresistance <flukyfactory@me.com>
2012-04-10 20:42:08 -0600

    Init
    Fluky Figthers push to Git

---
+++ flukyfigther/flukyfigther/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  flukyfigther
+//
+//  Created by Adolfo Gutierrez on 4/10/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
