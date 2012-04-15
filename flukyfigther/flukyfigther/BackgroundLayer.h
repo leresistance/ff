2c4deee5c3468f0af90522d968c03146bd4c0c18
leresistance <flukyfactory@me.com>
2012-04-15 00:26:36 -0600

    Background added

---
+++ flukyfigther/flukyfigther/BackgroundLayer.h
@@ -0,0 +1,13 @@
+//
+//  BackgroundLayer.h
+//  flukyfigther
+//
+//  Created by Adolfo Gutierrez on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+#import "cocos2d.h"
+#import "CCLayer.h"
+
+@interface BackgroundLayer : CCLayer
+
+@end
