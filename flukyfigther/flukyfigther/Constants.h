b484a590094fae76da8636767dd464c6a2c9e4de
LM <luis.matute@live.com>
2012-04-14 23:32:05 -0600

    Testing this Branch things
    getting the grip of this
    
    Signed-off-by: LM <luis.matute@live.com>

---
+++ flukyfigther/flukyfigther/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  flukyfigther
+//
+//  Created by Luis Matute on 4/14/12.
+//  Copyright (c) 2012 San Services. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Constants <NSObject>
+
+@end
