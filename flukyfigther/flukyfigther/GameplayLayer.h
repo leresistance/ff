919b04bb6e48dca0dff550d305cef61047f49153
leresistance <flukyfactory@me.com>
2012-04-15 00:46:01 -0600

    Gameplay Layer added

---
+++ flukyfigther/flukyfigther/GameplayLayer.h
@@ -0,0 +1,14 @@
+//
+//  GameplayLayer.h
+//  flukyfigther
+//
+//  Created by Adolfo Gutierrez on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "CCLayer.h"
+
+@interface GameplayLayer : CCLayer
+
+@end
---
+++ flukyfigther/flukyfigther/GameplayLayer.m
@@ -0,0 +1,13 @@
+//
+//  GameplayLayer.m
+//  flukyfigther
+//
+//  Created by Adolfo Gutierrez on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameplayLayer.h"
+
+@implementation GameplayLayer
+
+@end
