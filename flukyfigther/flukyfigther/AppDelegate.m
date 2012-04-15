a530b6db62c187ee58bde6ed444b625131f3e163
leresistance <flukyfactory@me.com>
2012-04-15 01:05:01 -0600

    GameScene.h added

--- flukyfigther/flukyfigther/AppDelegate.m
+++ flukyfigther/flukyfigther/AppDelegate.m
@@ -12,6 +12,7 @@
 #import "GameConfig.h"
 #import "HelloWorldLayer.h"
 #import "RootViewController.h"
+#import "GameScene.h"
 
 @implementation AppDelegate
 
@@ -70,8 +71,8 @@
 	[director setOpenGLView:glView];
 	
 //	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
-//	if( ! [director enableRetinaDisplay:YES] )
-//		CCLOG(@"Retina Display Not supported");
+if( ! [director enableRetinaDisplay:YES] )
+		CCLOG(@"Retina Display Not supported");
 	
 	//
 	// VERY IMPORTANT:
@@ -110,7 +111,8 @@
 	[self removeStartupFlicker];
 	
 	// Run the intro Scene
-	[[CCDirector sharedDirector] runWithScene: [HelloWorldLayer scene]];
+	//[[CCDirector sharedDirector] runWithScene: [HelloWorldLayer scene]];
+     [[CCDirector sharedDirector] runWithScene:[GameScene node]];
 }
 
 
