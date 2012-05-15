8051aaf114ff54860fa2d57177ce0870e3b66440
LM <luis.matute@live.com>
2012-05-14 19:59:30 -0600

    IDK
    Signed-off-by: LM <luis.matute@live.com>

---
+++ flukyfigther/flukyfigther/GameObject.h
@@ -0,0 +1,13 @@
+//
+//  GameObject.h
+//  flukyfigther
+//
+//  Created by Luis Matute on 4/17/12.
+//  Copyright (c) 2012 San Services. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface GameObject : CCSprite
+
+@end
---
+++ flukyfigther/flukyfigther/GameObject.m
@@ -0,0 +1,13 @@
+//
+//  GameObject.m
+//  flukyfigther
+//
+//  Created by Luis Matute on 4/17/12.
+//  Copyright (c) 2012 San Services. All rights reserved.
+//
+
+#import "GameObject.h"
+
+@implementation GameObject
+
+@end
